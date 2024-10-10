a8ee1313fe2b50b4ee96320b69d0b3d8d967c3f0
Mehmet Nuri Erhan <96691585+MehmetNuriErhan99@users.noreply.github.com>
2024-10-10 21:43:11 +0300

    DbContext States

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -11,11 +11,26 @@ using (var _context = new AppDbContext())
 //AppDbContext içerisine DbContextInitializer.OptionsBuilder.Options yazdığımızda birden fazla  db ile baglantı kuracağımızı söyler
 // ikinci bir overloadım olucak veritabanıyla ilgili optionsları belirteceğimiz sınıf belirtecegım 
 {
-    var products = await _context.Products.ToListAsync();
+    //efcore veritabanından ilk okuduğunda Unchanged 
+    //eğer memoriyede olmayan  bir şeyi okumaya calısırsa detached oluyor
+    var newProduct = new Product { Name = "Kalem 200", Price = 200,Stock = 100, Barcode = 333 };
 
-    products.ForEach(p =>
-    {
-        Console.WriteLine($"{p.Id} : {p.Name} : {p.Price} - {p.Price}");
-    });
+    Console.WriteLine($"İlk state:{ _context.Entry(newProduct).State}");
+
+    await _context.AddAsync( newProduct );
+    Console.WriteLine($"son state:{_context.Entry(newProduct).State}");
+
+    await _context.SaveChangesAsync();
+    Console.WriteLine($"state save changes:{_context.Entry(newProduct).State}");
+
+
+    //var products = await _context.Products.ToListAsync();
+
+    //products.ForEach(p =>
+    //{ 
+    //    var state = _context.Entry(p).State;
+
+    //    Console.WriteLine($"{p.Id} : {p.Name} : {p.Price} - {p.Price} State: {state}");
+    //});
 
 }
